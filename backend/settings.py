e548585886f878f55ab5fe581ace6861cccce0a2
theLE0NIDAS <divyanshpathak1964@gmail.com>
2023-07-16 15:13:30 +0530

    swagger UI installed

--- backend/settings.py
+++ backend/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
+
     'base',
     'student',
     'attendance',
--- backend/urls.py
+++ backend/urls.py
@@ -17,14 +17,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 # from rest_framework_simplejwt.views import (
 #     TokenObtainPairView,
 #     # TokenRefreshView,
 # )
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', include('base.urls')),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
