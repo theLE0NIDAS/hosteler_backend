0d0aba96dbdc45d9fd7c59ba757ef3331a9f0d52
theLE0NIDAS <divyanshpathak1964@gmail.com>
2023-08-08 21:50:11 +0530

    added room search by rollnumber api route update

--- room/urls.py
+++ room/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.api_room_list, name='room_list'),
     path('search/', views.api_room_search, name='room_search'),
     path('create/', views.api_room_create_floor, name='room_create_floor'),
-    path('search/<str:roll_number>/', views.api_room_detail_by_student, name='room_detail_by_student'),
     path('register/<int:room_number>/', views.api_room_student_register, name='room_student_register'),
     path('deregister/<int:room_number>/', views.api_room_student_deregister, name='room_student_deregister'),
     # path('update/<int:room_id>/', views.api_room_update, name='room_update'),
     path('delete/<int:room_number>/', views.api_room_delete, name='room_delete'),
     path('<int:room_number>/', views.api_room_detail, name='room_detail'),
+    path('<str:roll_number>/', views.api_room_detail_by_student, name='room_detail_by_student'),
 ]
